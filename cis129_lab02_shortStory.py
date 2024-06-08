a91ddfe37b20caa829645afe5d32ed322b54a6cf
AlecTerzan <aterzan@mail.pima.edu>
2024-06-08 10:57:57 -0700

    Update cis129_lab02_shortStory.py
    Changes print message 'Hello World' to print a short story.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,7 @@
-print('Hello World!')
+# Author: Alec Terzan
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+print('There was an honorable cowboy and a dishonorable outlaw')
+print('They dueled at noon, then killed each other.')
+print('The heart of the outlaw was filled with regret and hate')
+print('The heart of the cowboy was filled with love and peace')
