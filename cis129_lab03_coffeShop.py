050b7de329ce8dda4699b0c113d8f0d1c64e0f50
AlecTerzan <aterzan@mail.pima.edu>
2024-06-12 12:27:59 -0700

    Update cis129_lab03_coffeShop.py
    Added and updated code to display number of items at their cost and displays sum of the cost of items instead of number of items.

--- cis129_lab03_coffeShop.py
+++ cis129_lab03_coffeShop.py
@@ -12,25 +12,35 @@ value2 = input('Number of muffins bought?\n')
 value3 = input('Number of cookies bought?\n')
 #Set a fourth value of number of pastries bought
 value4 = input('Number of pastries bought?\n')
-print('********************************************************\n\n')
-print('********************************************************\n')
-# Print how many of each item is bought
+print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n\n')
+print('VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV\n')
+# Print how many of each item is bought and cost of each purchase
+print('Coffee Shop Emporium')
+newvalue1 = float(value1) * 5
+newvalue2 = float(value2) * 4
+newvalue3 = float(value3) * 3
+newvalue4 = float(value4) * 6
 if value1 == 1:
-  print(value1, 'Coffee bought\n')
+  print(value1, 'Coffee bought at $5: ', newvalue1,'\n')
 else:
-  print (value1, 'Coffees bought\n')
+  print (value1, 'Coffees bought at $5: ', newvalue1,'\n')
 if value2 == 1:
-  print(value2, 'Muffin bought\n')
+  print(value2, 'Muffin bought at $4: ', newvalue2,'\n')
 else:
-  print(value2, 'Muffins bought\n')
-
+  print(value2, 'Muffins bought at $4: ', newvalue2,'\n')
+if value3 == 1:
+  print(value3, 'Cookie bought at $3: ', newvalue3,'\n')
+else:
+  print(value3, 'Cookies bought at $3: ', newvalue3,'\n')
+if value4 == 1:
+  print(value4, 'Pastry bought at $6: ', newvalue4,'\n')
+else:
+  print(value4, 'Pastries bought at $6 ', newvalue4, '\n')
 # Calculate total including tax
-value1 = float(value1)
-value2 = float(value2)
-tax = sum(value1, value2, value3, value4) * 0.06
+tax = sum(newvalue1, newvalue2, newvalue3, newvalue4) * 0.06
 print('6% Tax: $ ', tax)
-total = sum(value1, value2, value3, value4, tax)
-print('-----')
+total = sum(newvalue1, newvalue2, newvalue3, newvalue4, tax)
+print('-----------')
 print('Total: ', total, '\n')
-print('********************************************************\n')
+print('<><><><><><><><><><><><><><><><><><><><><><><><>\n')
 print('Thank you for visiting and come back soon!\n')
