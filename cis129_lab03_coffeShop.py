79edb266c72e525c406dcc35a012341faa6e0975
AlecTerzan <aterzan@mail.pima.edu>
2024-06-12 12:14:07 -0700

    Update cis129_lab03_coffeShop.py
    Add two new items and a goodbye message to user

--- cis129_lab03_coffeShop.py
+++ cis129_lab03_coffeShop.py
@@ -1,5 +1,5 @@
 # Alec Terzan
-# 6/12/2024
+# Lab 03 for CIS 129
 # Print a reciept for a coffee shop using user input data and calculations to determine total cost
 
 print('********************************************************\n')
@@ -8,6 +8,10 @@ print('Coffe Shop Emporium \n')
 value1 = input('Number of coffees bought?\n')
 # Set a different value to the user input of muffins for total value
 value2 = input('Number of muffins bought?\n')
+# Set another value to the number of cookies bought
+value3 = input('Number of cookies bought?\n')
+#Set a fourth value of number of pastries bought
+value4 = input('Number of pastries bought?\n')
 print('********************************************************\n\n')
 print('********************************************************\n')
 # Print how many of each item is bought
@@ -20,10 +24,13 @@ if value2 == 1:
 else:
   print(value2, 'Muffins bought\n')
 
+# Calculate total including tax
 value1 = float(value1)
 value2 = float(value2)
-tax = sum(value1, value2) * 0.06
-total = tax + value1 + value2
+tax = sum(value1, value2, value3, value4) * 0.06
+print('6% Tax: $ ', tax)
+total = sum(value1, value2, value3, value4, tax)
 print('-----')
 print('Total: ', total, '\n')
 print('********************************************************\n')
+print('Thank you for visiting and come back soon!\n')
